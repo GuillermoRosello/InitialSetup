252ad8ba09e89ba8e47f6bf8ce25c1347e9e596b
Guillermo Rosello <Guillermo.Rosello@followoz.com>
2020-08-15 18:43:54 -0300

    added plugins and changed path

--- Initial Setup.ps1
+++ Initial Setup.ps1
@@ -1,4 +1,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install terraform -y
 choco install golang -y 
-choco install 
+go get github.com/Telmate/terraform-provider-proxmox/cmd/terraform-provider-proxmox
+go get github.com/Telmate/terraform-provider-proxmox/cmd/terraform-provisioner-proxmox
+cp go\bin\terraform-provider-proxmox.exe AppData\Roaming\terraform.d\plugins
+cp go\bin\terraform-provisioner-proxmox.exe AppData\Roaming\terraform.d\plugins
